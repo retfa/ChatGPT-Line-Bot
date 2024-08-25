8c96074d641043dedf4cf54d50fd174d81578795
retfa <retfa.tw@yahoo.com.tw>
2024-08-25 06:11:36 +0000

    test256256

--- src/models.py
+++ src/models.py
@@ -60,6 +60,6 @@ class OpenAIModel(ModelInterface):
         json_body = {
             "prompt": prompt,
             "n": 1,
-            "size": "256x512"
+            "size": "256x256"
         }
         return self._request('POST', '/images/generations', body=json_body)
